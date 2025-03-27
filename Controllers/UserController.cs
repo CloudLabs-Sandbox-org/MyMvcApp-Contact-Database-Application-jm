3a871ed81c97e763c116f3ce8e7b611561138a32
JUAN MORI TENARIS <github_cloudlabsuser_1972@cloudlabsaiuser.com>
2025-03-27 16:39:13 +0000

    commit 3

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -8,95 +8,92 @@ public class UserController : Controller
 {
     public static System.Collections.Generic.List<User> userlist = new System.Collections.Generic.List<User>();
 
-    // GET: User
-    public ActionResult Index()
-    {
-        return View(userlist);
-    }
-
-    // GET: User/Details/5
-    public ActionResult Details(int id)
-    {
-        var user = userlist.FirstOrDefault(u => u.Id == id);
-        if (user == null)
+        // GET: User
+        public ActionResult Index()
         {
-            return NotFound();
-        }
-        return View(user);
-    }
-
-    // GET: User/Create
-    public ActionResult Create()
-    {
-        return View();
-    }
-
-    // POST: User/Create
-    [HttpPost]
-    public ActionResult Create(User user)
-    {
-        if (ModelState.IsValid)
-        {
-            user.Id = userlist.Count > 0 ? userlist.Max(u => u.Id) + 1 : 1;
-            userlist.Add(user);
-            return RedirectToAction(nameof(Index));
-        }
-        return View(user);
-    }
-
-    // GET: User/Edit/5
-    public ActionResult Edit(int id)
-    {
-        var user = userlist.FirstOrDefault(u => u.Id == id);
-        if (user == null)
-        {
-            return NotFound();
-        }
-        return View(user);
-    }
-
-    // POST: User/Edit/5
-    [HttpPost]
-    public ActionResult Edit(int id, User user)
-    {
-        var existingUser = userlist.FirstOrDefault(u => u.Id == id);
-        if (existingUser == null)
-        {
-            return NotFound();
+            var users = userlist;
+            return View(users);
         }
 
-        if (ModelState.IsValid)
+        // GET: User/Details/5
+        public ActionResult Details(int id)
         {
-            existingUser.Name = user.Name;
-            existingUser.Email = user.Email;
-            // Update other fields as necessary
-            return RedirectToAction(nameof(Index));
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
         }
-        return View(user);
-    }
 
-    // GET: User/Delete/5
-    public ActionResult Delete(int id)
-    {
-        var user = db.Users.Find(id); // Busca el usuario en la base de datos
-        if (user == null)
+        // GET: User/Create
+        public ActionResult Create()
         {
-            return HttpNotFound();
+            return View();
         }
-        return View(user);
-    }
 
-    // POST: User/Delete/5
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Delete(int id, FormCollection collection)
-    {
-        var user = db.Users.Find(id); // Busca el usuario en la base de datos
-        if (user != null)
+        // POST: User/Create
+        [HttpPost]
+        public ActionResult Create(User user)
         {
-            db.Users.Remove(user); // Elimina el usuario
-            db.SaveChanges(); // Guarda los cambios
+            if (ModelState.IsValid)
+            {
+                userlist.Add(user);
+                return RedirectToAction("Index");
+            }
+            return View(user);
         }
-        return RedirectToAction("Index");
-    }
+
+        // GET: User/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        // POST: User/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, User user)
+        {
+            var existingUser = userlist.FirstOrDefault(u => u.Id == id);
+            if (existingUser == null)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                existingUser.Name = user.Name;
+                existingUser.Email = user.Email;
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        // GET: User/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        // POST: User/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            userlist.Remove(user);
+            return RedirectToAction("Index");
+        }
 }
