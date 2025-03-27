10b04fbf0e150b2e3fc95b3b6b8a7aa3231078f0
JUAN MORI TENARIS <github_cloudlabsuser_1972@cloudlabsaiuser.com>
2025-03-27 16:53:08 +0000

    commit 4

--- obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fe03b7dad2c1a93c9694aefa3730ebb6c4c5059")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a871ed81c97e763c116f3ce8e7b611561138a32")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
